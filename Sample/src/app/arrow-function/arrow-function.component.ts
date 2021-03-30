29002ca684f3a465c0c6c956e74c5a299b7fd5b2
Musaddik Shaikh <musaddikshaikh5@gmail.com>
2021-03-30 12:31:57 +0530

    arrow-function

---
+++ Sample/src/app/arrow-function/arrow-function.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-arrow-function',
+  templateUrl: './arrow-function.component.html',
+  styleUrls: ['./arrow-function.component.scss']
+})
+export class ArrowFunctionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
