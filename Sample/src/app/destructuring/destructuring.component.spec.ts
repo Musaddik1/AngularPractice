945f4da3d4b020e2d712fa335f74a4bdd11b53c1
Musaddik Shaikh <musaddikshaikh5@gmail.com>
2021-03-30 12:34:09 +0530

    destructuring component

---
+++ Sample/src/app/destructuring/destructuring.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DestructuringComponent } from './destructuring.component';
+
+describe('DestructuringComponent', () => {
+  let component: DestructuringComponent;
+  let fixture: ComponentFixture<DestructuringComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DestructuringComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DestructuringComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
